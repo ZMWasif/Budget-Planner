82e2b7093e6bdbc8533a3ed62090d6793446bacd
Wasif004 <ziauddinwasif@gmail.com>
2024-04-18 02:02:12 +0200

    Hosting

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+    { path: '', redirectTo: '/budget-planner/login', pathMatch: 'full' },
     {
         path: 'budget-planner',
         loadChildren: () => import('./budget-planner/budget-planner.module')
